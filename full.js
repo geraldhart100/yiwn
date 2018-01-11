a18044d6479dbbda5632bba23fdcf9b424a70496
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2018-01-11 14:41:35 +0400

    Expose extra modules in `full`

--- full.js
+++ full.js
@@ -4,3 +4,7 @@ const RA = require('ramda-adjunct')
 const Y = require('./lib')
 
 module.exports = Object.assign({}, Y, RA, R)
+
+module.slug = require('yiwn-slug')
+module.shortid = require('yiwn-shortid')
+
